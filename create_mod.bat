ba7ac7b0bbfd7bd8794c6647eed52b83f1c3ec2e
rita123dffdg <aouirita@gmail.com>
2025-07-25 15:16:53 +0100

    Create create_mod.bat

---
+++ create_mod.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Creating MCA Companion Mod project...
+
+mkdir mca_companion
+cd mca_companion
+mkdir src\main\java\com\mca\companion
+mkdir src\main\resources\META-INF
+mkdir src\main\resources\assets\mca_companion\lang
+
+echo Project structure created!
+echo Now you need to copy the code files into the folders.
+pause
